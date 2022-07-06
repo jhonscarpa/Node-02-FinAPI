1d88c86465e23067eec20fe2e661bd8e0a50ca5f
jhonscarpa <scarpa.jhonatan@outlook.com>
2022-07-05 21:47:09 -0300

    create account

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 const express = require('express')
+const { v4: uuidv4 } = require('uuid')
 
 const app = express()
+
+app.use(express.json())
+
+const customers = []
+
+app.post('/account', (request, response) => {
+  const { cpf, name } = request.body
+
+  const customerAlreadyExists = customers.some(customer => customer.cpf === cpf)
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ message: 'CPF ja cadastrado' })
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: [],
+  })
+
+  return response.status(201).send()
+})
 
 app.listen(8080)
 console.log('Server started on port 8080 🚀')
